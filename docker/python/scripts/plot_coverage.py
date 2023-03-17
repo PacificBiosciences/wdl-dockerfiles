b6a72f9057bce65ed3966435f71dc6aa6a15009b
Heather Ward <heather@dnastack.com>
2023-03-17 17:24:45 -0400

    Add argparse to plot_coverage

--- docker/python/scripts/plot_coverage.py
+++ docker/python/scripts/plot_coverage.py
@@ -1,24 +1,68 @@
+#!/usr/bin/env python3
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from argparse import ArgumentParser
 
-DPI=200
 
-coverage = pd.read_csv( snakemake.input[0] )
+def _parse_args():
+    parser = ArgumentParser(description="Plot coverage data for a sample")
 
-order = sorted(coverage.target.unique())
-maxCols = int( len(order) ** 0.5 ) + 1
+    parser.add_argument(
+        "-t",
+        "--targets-base-coverage-csv",
+        type=str,
+        help="Coverage CSV for sample across target regions bed",
+        required=True,
+    )
+    parser.add_argument(
+        "-o",
+        "--out-dir",
+        type=str,
+        help="Output directory to save files into",
+        default=".",
+    )
 
-plt.figure(figsize=(40,40))
+    args = parser.parse_args()
+    return args
 
-g = sns.FacetGrid(data=coverage,sharex=False,
-                  col='target',col_wrap=maxCols,col_order=order,
-                  hue='target')
 
-g.map(plt.plot, 'start','coverage')
+def main():
+    try:
+        target_base_coverage_csv = snakemake.input[0]
+        out_dir = snakemake.params.odir
+    except NameError:
+        args = _parse_args()
+        target_base_coverage_csv = args.target_base_coverage_csv
+        out_dir = args.out_dir
 
-g.set_xlabels('chr start pos')
-g.set_ylabels('Coverage')
+    DPI = 200
 
-g.savefig(f'{snakemake.params.odir}/coverage_by_target.png', dpi=DPI)
+    coverage = pd.read_csv(target_base_coverage_csv)
+
+    order = sorted(coverage.target.unique())
+    maxCols = int(len(order) ** 0.5) + 1
+
+    plt.figure(figsize=(40, 40))
+
+    g = sns.FacetGrid(
+        data=coverage,
+        sharex=False,
+        col="target",
+        col_wrap=maxCols,
+        col_order=order,
+        hue="target",
+    )
+
+    g.map(plt.plot, "start", "coverage")
+
+    g.set_xlabels("chr start pos")
+    g.set_ylabels("Coverage")
+
+    g.savefig(f"{out_dir}/coverage_by_target.png", dpi=DPI)
+
+
+if __name__ == "__main__":
+    main()
